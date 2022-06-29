03742647078e2b3d51287c9fc62b60b413f63dfa
ninomiya-hibiki <ooi2172054@stu.o-hara.ac.jp>
2022-06-29 12:17:28 +0900

    テキストP160まで

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,40 +13,13 @@ class HelloController extends Controller
   
    public function index(Request $request)
    {
-       return view('hello.index', ['msg'=>'フォームを入力：']);
+       return view('hello.index', ['msg'=>'フォームを入力下さい。']);
    }
 
-
-   public function post(Request $request)
-{
-   $rules = [
-       'name' => 'required',
-       'mail' => 'email',
-       'age' => 'numeric',
-   ];
-   $messages = [
-       'name.required' => '名前は必ず入力して下さい。',
-       'mail.email'  => 'メールアドレスが必要です。',
-       'age.numeric' => '年齢は整数で記入下さい。',
-       'age.min' => '年齢はゼロ歳以上で記入下さい。',
-       'age.max' => '年齢は200歳以下で記入下さい。',
-   ];
-   $validator = Validator::make($request->all(), $rules, $messages);
-
-   $validator->sometimes('age', 'min:0', function($input){
-       return !is_int($input->age);
-   });
-   $validator->sometimes('age', 'max:200', function($input){
-       return !is_int($input->age);
-   });
-
-   if ($validator->fails()) {
-       return redirect('/hello')
-           ->withErrors($validator)
-           ->withInput();
+   public function post(HelloRequest $request)
+   {
+       return view('hello.index', ['msg'=>'正しく入力されました！']);
    }
-   return view('hello.index', ['msg'=>'正しく入力されました！']);
-}
 
 }
-?>
+
