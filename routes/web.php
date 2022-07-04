2101d618936a24d1a6631bc2f3c9bc9473e89d96
ninomiya-hibiki <ooi2172054@stu.o-hara.ac.jp>
2022-07-04 12:18:21 +0900

    テキストP212 wherRawまで

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,5 @@ Route::post('hello/edit', 'HelloController@update');
 //データの削除
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
+
+Route::get('hello/show', 'HelloController@show');
