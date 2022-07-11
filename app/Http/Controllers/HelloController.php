f9712c53887e2a0ebf0d325eb4c72c62002368b8
ninomiya-hibiki <ooi2172054@stu.o-hara.ac.jp>
2022-07-11 12:35:24 +0900

    paginote

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -21,7 +21,7 @@ class HelloController extends Controller
       $sort='age';
    }
    $items = Person::orderBy($sort, 'asc')
-      ->simplePaginate(5);
+      ->paginate(5);
    $param = ['items' => $items, 'sort' => $sort];
    return view('hello.index', $param);
 }
