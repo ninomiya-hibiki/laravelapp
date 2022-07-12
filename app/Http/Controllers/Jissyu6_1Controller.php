d2e5bf4f6fcda9d293777dfb5f72c01a23a003c0
ninomiya-hibiki <ooi2172054@stu.o-hara.ac.jp>
2022-07-12 11:36:02 +0900

    実習6_1

---
+++ app/Http/Controllers/Jissyu6_1Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Person;
+use Illuminate\Http\Request;
+
+class Jissyu6_1Controller extends Controller
+{
+    public function index(Request $request)
+    {
+        $items = Person::all();
+        $param = ['input'=>$request->input,'items'=>$items];
+        return view('jissyu6_1.index', $param);
+    }
+    public function find(Request $request)
+    {
+        $item = Person::where('name',$request->input)->first();
+        return view('jissyu6_1.show', ['item' => $item]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -78,3 +78,7 @@ Route::get('board', 'BoardController@index');
 
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create');
+
+//6_1
+Route::get('jissyu13', 'Jissyu6_1Controller@index');
+Route::post('/jissyu13/find', 'jissyu6_1Controller@find');
